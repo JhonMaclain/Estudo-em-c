3209a9c1ecc5886569e5ab2ff13e98e67b73ad2c
João Pedro Lacerda <128433811+JhonMaclain@users.noreply.github.com>
2023-04-14 11:04:04 -0300

    Update salario-h-f.c

--- salario-h-f.c
+++ salario-h-f.c
@@ -5,7 +5,7 @@
 Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
-//a = altura e pi = pso ideal
+
 #include <stdio.h>
 
 int main()
